b64bdd6bd768a55e277d1854cffc116a23dc2a80
shahjalalcse <shah-jalal1>
2021-04-12 16:31:54 +0600

    problem solved

--- index.js
+++ index.js
@@ -21,11 +21,20 @@ client.connect(err => {
 
     app.post('/addAppointment', (req, res) => {
         const appointment = req.body;
-        console.log(appointment);
+        // console.log(appointment);
         appointmentCollection.insertOne(appointment)
             .then(result => {
                 res.send(result.insertedCount > 0)
             })
+    });
+
+    app.post('/addAppointmentsByDate', (req, res) => {
+        const date = req.body;
+        console.log(date.date);
+        appointmentCollection.find({date: date.date}) 
+        .toArray((err, document)  => {
+            res.send(document)
+        })
     });
 });
 
